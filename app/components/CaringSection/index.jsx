bbac68310e4bb6a34b936721e67891d9eeffcdff
Kamron-Ems <kamrondev1@gmail.com>
2026-04-03 09:27:13 +0100

    ./

--- app/components/CaringSection/index.jsx
+++ app/components/CaringSection/index.jsx
@@ -43,7 +43,10 @@ function CaringSection() {
     >
         {/* title */}
         <h1 className='h1 text-center ' >Caring is the new marketing</h1>
-        <p className='text-gray-600 max-w-[550px] text-center mx-auto antialiased' >The Nextcent blog is the best place to read about the latest membership insights, trends and more. See who's joining the community, read about how our community are increasing their membership income and lot's more. </p>
+        {/* <p className='text-gray-600 max-w-[550px] text-center mx-auto antialiased' >The Nextcent blog is the best place to read about the latest membership insights, trends and more. See who's joining the community, read about how our community are increasing their membership income and lot's more. </p> */}
+        <p className='text-gray-600 max-w-[550px] text-center mx-auto antialiased'>
+            The Nextcent blog is the best place to read about the latest membership insights, trends and more. See who&apos;s joining the community, read about how our community are increasing their membership income and lots more.
+        </p>
       </motion.div>
       {/* card section */}
       <div className='grid gap-y-36 lg:grid-cols-3 grid-cols-1 md:grid-cols-2 place-items-center justify-items-center items-center gap-x-60  px-32 antialiased '>
